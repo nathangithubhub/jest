c311e5b79c1703c8e527f20c42a4e2d52c954427
Nathan Li <nathan.li@healthfidelity.com>
2018-06-01 15:03:31 -0700

    try new things

--- examples/getting-started/sum.test.js
+++ examples/getting-started/sum.test.js
@@ -5,3 +5,9 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+test("adds 3 + 3 to equal 6", () => {
+  expect(sum(3, 3)).toEqual(6);
+});
+test("adds 1 + 1 should return as a number", () => {
+  expect(sum(1, 1)).toBeCalled
+})
